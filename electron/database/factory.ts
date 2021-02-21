b1d683a789160c9ee799b2eabe7671135885a61d
sushat4692 <sush@sus-happy.net>
2021-02-21 18:39:10 +0800

    Arranged for building package and fix version number

--- electron/database/factory.ts
+++ electron/database/factory.ts
@@ -1,10 +1,14 @@
 import { app } from "electron";
 import Database from "nedb-promises";
 import isDev from "electron-is-dev";
+import path from "path";
 
 const dbFactory = (fileName, isOnMemory = false) =>
     Database.create({
-        filename: `${isDev ? "." : app.getAppPath()}/data/${fileName}`,
+        filename: path.join(
+            isDev ? "." : app.getPath("userData"),
+            `data/${fileName}`
+        ),
         timestampData: true,
         autoload: true,
         inMemoryOnly: isOnMemory,
--- electron/main.ts
+++ electron/main.ts
@@ -11,7 +11,7 @@ async function createWindow() {
         },
     });
 
-    win.loadFile("index.html");
+    win.loadFile(path.join(__dirname, "index.html"));
     // win.webContents.openDevTools();
 }
 
